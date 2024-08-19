d1a68309cb2c1f59fc04fdfb6b42013f94531839
Alexis Weinreb <alexis.weinreb@yale.edu>
2024-08-19 16:22:35 -0400

    correct download script

--- src/download_kaletsky_2018.sh
+++ src/download_kaletsky_2018.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
-#SBATCH --partition=general
+#SBATCH --partition=day
 #SBATCH --job-name=down_sra_kal
 #SBATCH -c 1
 #SBATCH --mem=5G
-#SBATCH --time=5:00:00
+#SBATCH --time=15:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alexis.weinreb@yale.edu
 
+set -ue
+
 echo "Starting $(date)"
+
 module load SRA-Toolkit
 
 
-mapfile sampleList < <(cat sampleList | cut -f 1)
+mapfile sampleList < <( cat metadata/sample_list_kaletsky_2018.csv | cut -f 1 -d"," | tail -n +2 )
+
+echo "${sampleList[@]}"
+echo
 
 for sample in "${sampleList[@]}"
 do
-echo "Downloading $sample"
-fastq-dump --split-files $sample
-echo
-echo
+  echo "Downloading $sample"
+  fastq-dump --split-files $sample
+  echo
+  echo
 done
 
 echo "Done $(date)"
+
