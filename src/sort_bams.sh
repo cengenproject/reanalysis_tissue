33bbc5c6bc360286581c48c253d39e46dc05f9d3
AlexWeinreb <alexis.weinreb@yale.edu>
2024-08-27 10:36:59 -0400

    Sort and index: create

---
+++ src/sort_bams.sh
@@ -0,0 +1,37 @@
+
+
+set -ue
+
+echo "Starting $(date)"
+
+start=$1
+echo "Start at $start"
+
+in_dir="/vast/palmer/scratch/hammarlund/aw853/2408_reanalysis/bams_unsorted"
+out_dir="/vast/palmer/scratch/hammarlund/aw853/2408_reanalysis/bams_sorted"
+
+sampleList=( $(ls $in_dir/*.bam) )
+
+sampleList=(${sampleList[@]:$start:4})
+
+echo "${#sampleList[@]} samples: ${sampleList[@]}"
+
+
+for sample in ${sampleList[@]}
+do
+  echo "$sample"
+  samtools sort \
+    -@ 5 \
+    -O bam \
+    -o $out_dir/$sample \
+    $in_dir/$sample
+  
+  samtools index -@ 5 $out_dir/sample
+  
+done
+
+
+echo "Done $(date)"
+
+
+
