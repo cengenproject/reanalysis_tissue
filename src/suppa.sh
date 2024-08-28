63a14e39a6604ba95b1875e1e3b0f1248a1fdff0
AlexWeinreb <alexis.weinreb@yale.edu>
2024-08-28 14:11:16 -0400

    SUPPA

---
+++ src/suppa.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --partition=day
+#SBATCH --job-name=suppa_psiEvent
+#SBATCH -c 1
+#SBATCH --mem=8G
+#SBATCH --time=30:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alexis.weinreb@yale.edu
+
+set -ue
+
+echo "Start $(date)"
+
+
+module load miniconda
+conda activate SUPPA2
+
+
+WS="WS289"
+
+
+events_file="../suppa_events/data/events/${WS}_all_strict.ioe"
+tx_tpm="intermediates/240827_strq_outs/240828_tx_TPM.tsv"
+
+out_dir="intermediates/240828_psiPerEvent"
+
+mkdir -p $out_dir
+
+
+# Note we gathered all events in single file with:
+# head -1 WS289_A3_strict.ioe > ../WS289_all_strict.ioe
+# cat *.ioe | sed '/^seqname/d' >> ../WS289_all_strict.ioe
+
+
+suppa.py psiPerEvent \
+    --mode INFO \
+    --ioe-file $events_file \
+    --expression-file $tx_tpm \
+    --total-filter 10 \
+    --output-file $out_dir
+
+
+echo "End $(date)"
+
+
+
+
